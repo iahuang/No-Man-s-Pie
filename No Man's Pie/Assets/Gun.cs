fdffd2167959f9dc63cfb044c85345129d248a8b
iahuang <ianhuang@Bag-End.local>
2016-08-18 07:27:34 -0400

    stuff1222

---
+++ No Man's Pie/Assets/Gun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	public int cdtime = 1000;
+	public int cdown = 0;
+	public GameObject bullet;
+	public float damage = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject nbullet = Instantiate (bullet, transform.position + transform.forward, transform.rotation) as GameObject;
+		nbullet.rigidbody.velocity = transform.forward * 32;
+	}
+}
